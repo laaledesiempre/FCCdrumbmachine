174a1947d488a0ae418b1a97781854279674f82c
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-04-24 13:46:36 -0300

    beta| modified structure and function

--- src/components/main/DrumPad.jsx
+++ src/components/main/DrumPad.jsx
@@ -3,11 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { setDisplayContent } from "../../store/slices/display";
 
 export const DrumPad = () => {
+  const audiofiles={
+    heater1: {name:"Heater 1",src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"},
+    heater2: {name:"Heater 2",src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"},
+    heater3: {name:"Heater 3",src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"},
+    heater4: {name:"Heater 4",src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"},
+    clap: {name:"Clap",src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
+    openhh: {name:"Open HH",src:"https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"},
+    kicknhat: {name:"Kick n' Hat",src:"https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"},
+    kick:{name:"Kick",scr:"https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"},
+    closedhh: {name:"Closed HH",src:"https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"},
+  }
   const text= useSelector((state)=> state.displayReducer.display)
   const dispatch= useDispatch()
   const play=(name,src)=>{
       dispatch(setDisplayContent(name))
-      return new Audio(src).play()
+      document.querySelector(src).play()
   }
   window.addEventListener("keydown",
   (event) => {
@@ -44,15 +55,42 @@ export const DrumPad = () => {
   return (
     <div>
       <div className="drums-container" id="drum-machine">
-        <button className="drumb-item" onClick={()=>{play("Heater 1","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3")}} id="Q">Q</button>
-        <button className="drumb-item" onClick={()=>{play("Heater 2","https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3")}} id="W">W</button>
-        <button className="drumb-item" onClick={()=>{play("Heater 3","https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3")}} id="E">E</button>
-        <button className="drumb-item" onClick={()=>{play("Heater 4","https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3")}} id="A">A</button>
-        <button className="drumb-item" onClick={()=>{play("Clap","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")}} id="S">S</button>
-        <button className="drumb-item" onClick={()=>{play("Open HH","https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3")}} id="D">D</button>
-        <button className="drumb-item" onClick={()=>{play("Kick n' Hat","https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3")}} id="Z">Z</button>
-        <button className="drumb-item" onClick={()=>{play("Kick","https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3")}} id="X">X</button>
-        <button className="drumb-item" onClick={()=>{play("Closed HH","https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")}} id="C">C</button>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.heater1.name,"Q")}}>Q</button>
+          <audio src={audio.heater1.src} id="Q"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.heater2.name,"W")}}>W</button>
+          <audio src={audio.heater2.src} id="W"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.heater3.name,"E")}}>E</button>
+          <audio src={audio.heater3.src} id="E"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.heater4.name,"A")}}>A</button>
+          <audio src={audio.heater4.src} id="A"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.clap.name,"S")}}>S</button>
+          <audio src={audio.clap.src} id="S"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.openhh.name,"D")}}>D</button>
+          <audio src={audio.openhh.src} id="D"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.kicknhat.name,"Z")}}>Z</button>
+          <audio src={audio.kicknhat.src} id="Z"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.kick.name,"X")}}>X</button>
+          <audio src={audio.kick.src} id="X"></audio>
+        </div>
+        <div className="drum-pad">
+        <button className="drumb-item" onClick={()=>{play(audio.closedhh.name,"C")}}>C</button>
+          <audio src={audio.closedhh.src} id="C"></audio>
+        </div>
         <div className="display">{text}</div>
       </div>
     </div>
