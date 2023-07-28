53118e6a832b7f10e7e50a6e7ee0016aba7e6c21
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-07-27 22:04:07 -0300

    vite version store fixed

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { displayReducer } from "./slices/display";
+
+export const store = configureStore({
+  reducer: {
+    displayReducer,
+  }
+})
