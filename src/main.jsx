93de0eba7caa5bb90b3db41f8545b9f1ca93b2fd
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-07-27 22:00:51 -0300

    vite version main fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./styles/index.scss";
+import { App } from "./components/App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
 )
+  ;
